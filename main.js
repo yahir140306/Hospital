8b33d0285e50085cb197354723201d10cf9e678b
yahir140306 <joseyahirhernandez11@gmail.com>
2023-11-17 18:18:33 -0500

    Agregando nuevos archivos y modificaciones en el diseño.

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-ScrollReveal().reveal('.cards');
-ScrollReveal().reveal('#card1', { delay: 200 });
-ScrollReveal().reveal('#card2', { delay: 400 });
-ScrollReveal().reveal('#card3', { delay: 600 });
-ScrollReveal().reveal('#card4', { delay: 800 });
-ScrollReveal().reveal('#card5', { delay: 1000 });
-ScrollReveal().reveal('#card6', { delay: 1200 });
-
-ScrollReveal().reveal('.noticia', { delay: 500 });
-
 let previousTitle = document.title
 
 window.addEventListener(`blur`, () => {
@@ -19,3 +9,15 @@ window.addEventListener(`focus`, () => {
     previousTitle = document.title
     document.title = "Hospital Chakalito"
 })
+
+////
+
+ScrollReveal().reveal('.cards');
+ScrollReveal().reveal('#card1', { delay: 200 });
+ScrollReveal().reveal('#card2', { delay: 400 });
+ScrollReveal().reveal('#card3', { delay: 600 });
+ScrollReveal().reveal('#card4', { delay: 800 });
+ScrollReveal().reveal('#card5', { delay: 1000 });
+ScrollReveal().reveal('#card6', { delay: 1200 });
+
+ScrollReveal().reveal('.noticia', { delay: 500 });
