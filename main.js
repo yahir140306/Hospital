f834af3ca7136ac736661c2ac4192aefb8b0e6d9
yahir140306 <joseyahirhernandez11@gmail.com>
2023-11-17 19:18:30 -0500

    Actualizacion del diseño y semantica

--- main.js
+++ main.js
@@ -19,5 +19,7 @@ ScrollReveal().reveal('#card3', { delay: 600 });
 ScrollReveal().reveal('#card4', { delay: 800 });
 ScrollReveal().reveal('#card5', { delay: 1000 });
 ScrollReveal().reveal('#card6', { delay: 1200 });
+ScrollReveal().reveal('#card7', { delay: 1400 });
+ScrollReveal().reveal('#card8', { delay: 1600 });
 
 ScrollReveal().reveal('.noticia', { delay: 500 });
